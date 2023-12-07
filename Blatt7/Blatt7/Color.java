4b4cec3d27b5900880684417d3dba057d1583e0d
Zakaria Bouzouf <zakaria.bouzouf@gmail.com>
2023-12-07 22:21:53 +0100

    Blatt8-1

---
+++ Blatt7/Blatt7/Color.java
@@ -0,0 +1,11 @@
+public enum Color {
+    BLACK,
+    RED,
+    ORANGE,
+    WHITE;
+
+    // public String toString(){
+    //     return String.format("%s",COLOR);
+    // }
+
+}
---
+++ Blatt8/SimpleLogger.java
@@ -0,0 +1,23 @@
+public class SimpleLogger implements Logger {
+    private static SimpleLogger uniqueLog = new SimpleLogger();
+
+    private SimpleLogger() {
+    };
+
+    public static SimpleLogger getLog() {
+        return uniqueLog;
+    }
+
+    public void log(LogLevel severity, String message) {
+        switch (severity) {
+            case DEBUG:
+                System.err.println("Log Level: Debug, The message: " + message);
+                break;
+            case INFO:
+                System.err.println("Log Level: Info, The message: " + message);
+                break;
+
+        }
+
+    }
+}
